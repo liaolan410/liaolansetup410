2b7402667d8ae0391ebbb51cdc3dc5252557ebc1
liaolan410 <unwellfletex@gmail.com>
2026-07-30 12:14:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -85,4 +85,15 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 client.login(process.env.TOKEN);
